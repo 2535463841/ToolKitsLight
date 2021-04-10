330ff85d65679762fc3da9f87e6d97928b83c5e3
zbw <2535463841@qq.com>
2021-04-10 23:48:34 +0800

    rename fluenthttpfs

--- core/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-
-setup(name='wetool',
-      version='0.1',
-      description='python toolkits',
-      author='zbw',
-      packages=find_packages())
